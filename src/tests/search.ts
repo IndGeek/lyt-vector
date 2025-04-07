26a4bd33feb5562a49372a000e6224191bb9c78c
lifeofsoumya <mondalsoumya2001@gmail.com>
2025-04-07 13:36:23 +0530

    feat: embedding & search

--- src/tests/search.ts
+++ src/tests/search.ts
@@ -6,7 +6,6 @@ const store = new VectorStore();
 async function similaritySearch(text: string): Promise<void> {
     try {
         const embedding = await generateEmbedding(text);
-        // console.log('Generated embedding for :', text, embedding);
         const results = store.search(embedding, { topK: 1 });
         console.log('Found results for ', results);
     } catch (error) {
